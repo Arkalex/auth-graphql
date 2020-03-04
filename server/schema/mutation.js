b290bef4bb29f543e8ee300a31e2e09c052932ab
Alex <aayllon@pasiona.com>
2020-03-04 12:37:02 +0100

    added mutation

--- server/schema/mutation.js
+++ server/schema/mutation.js
@@ -1,6 +1,26 @@
-const graphql = require('graphql');
-
+const graphql = require('graphql')
 const {
     GraphQLObjectType,
     GraphQLString
 } = graphql;
+
+const AuthService = require('../services/auth');
+const UserType = require('./types/user_type');
+
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        signup: {
+            type: UserType,
+            args: {
+                email: { type: GraphQLString },
+                password: { type: GraphQLString }
+            },
+            resolve(parentValue, {email, password}, req) {
+                return AuthService.signup({ email, password, req });
+            }
+        }
+    }
+});
+
+module.exports = mutation;
