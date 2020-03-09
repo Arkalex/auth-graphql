83f8f2bf5de3b76cabded1b7aa15faca9bdfd78e
Alex <aayllon@pasiona.com>
2020-03-09 13:52:55 +0100

    no message

--- server/schema/mutation.js
+++ server/schema/mutation.js
@@ -19,7 +19,26 @@ const mutation = new GraphQLObjectType({
             resolve(parentValue, {email, password}, req) {
                 return AuthService.signup({ email, password, req });
             }
-        }
+        },
+        logout: {
+            type: UserType,
+            resolve(parentValue, args, req) {
+                const { user } = req;
+                req.logout();
+
+                return user;
+            }
+        },
+        login: {
+            type: UserType,
+            args: {
+                email: { type: GraphQLString },
+                password: { type: GraphQLString }
+            },
+            resolve(parentValue, {email, password}, req) {
+                return AuthService.login({ email, password, req });
+            }
+        },
     }
 });
 
