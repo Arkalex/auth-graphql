b51f6b052be873a5acce1417f1a250ee7cc8002d
Alex <aayllon@pasiona.com>
2020-03-09 15:50:27 +0100

    logout mutation

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
+import { Link } from 'react-router';
+import mutation from '../mutations/Logout';
 
 class Header extends Component {
+
+    onLogoutClick() {
+        this.props.mutate({
+            refetchQueries: [{ query }]
+        });
+    }
+
     renderButtons() {
         const { loading, user } = this.props.data;
-
+        
         if(loading) {
             return <div></div>;
         }
 
         if(user) {
-            return <div>Logout</div>;
+            return (
+                <li>
+                    <a onClick={this.onLogoutClick.bind(this)}>Logout</a>
+                </li>
+            );
         } else {
             return (
                 <div>
-                    You're not signed in.
+                    <li>
+                        <Link to="/signup">Sign up</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
                 </div>
             );
         }
@@ -26,11 +44,18 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    {this.renderButtons()}
+                    <Link to="/" className="brand-logo left">
+                        Home
+                    </Link>
+                    <ul className="right">
+                        {this.renderButtons()}
+                    </ul>
                 </div>
             </nav>
         );
     }
 }
 
-export default graphql(query)(Header);
+export default graphql(mutation)(
+    graphql(query)(Header)
+);
